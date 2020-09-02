d885d846f1600759ff6207fa5190926e1ecc30e8
Lechen Tan <tan.lechen@gmail.com>
2020-09-01 20:11:03 -0400

    camera function

--- tool-savvy/frontend/src/pages/Tab3.tsx
+++ tool-savvy/frontend/src/pages/Tab3.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+  IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const { photos,takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -37,6 +42,21 @@ const Tab3: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 3 page" />
+          	<IonGrid>
+				<IonRow>
+				{photos.map((photo, index) => (
+					<IonCol size="6" key={index}>
+					<IonImg src={photo.base64 ?? photo.webviewPath} />
+					</IonCol>
+				))}
+				</IonRow>
+      		</IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+			<IonFabButton onClick={() => takePhoto()}>
+				<IonIcon icon={camera}></IonIcon>
+			</IonFabButton>
+        </IonFab>
+
       </IonContent>
     </IonPage>
   );
